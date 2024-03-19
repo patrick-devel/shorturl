b60bd9da99c03a080629097908d9f8c3cf647ffd
kspopova <kspopova@domclick.ru>
2024-03-19 18:43:39 +0300

    iter1: прописаны маршруты

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/patrick-devel/shorturl/internal/handlers"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handlers.MakeShortLink)
+	mux.HandleFunc("/{id}", handlers.RedirectShortLink)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
