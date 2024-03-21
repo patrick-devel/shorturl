47b68bf6ff7968ee8111c61c3463dd3dfa482fb2
kspopova <kspopova@domclick.ru>
2024-03-21 17:09:31 +0300

    iter4:  добавление конфига и флагов

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,20 +1,28 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 
+	"github.com/patrick-devel/shorturl/config"
 	"github.com/patrick-devel/shorturl/internal/handlers"
 )
 
 func main() {
+	addr, template := ParseFlag()
+	cfg := config.
+		NewConfigBuilder().
+		WithAddress(addr.String()).
+		WithTemplateLink(template.url).
+		Build()
+
 	mux := gin.New()
-	mux.POST("/", handlers.MakeShortLink)
-	mux.GET("/:id", handlers.RedirectShortLink)
+	mux.POST("/", handlers.MakeShortLink(&cfg))
+	mux.GET(fmt.Sprintf("%s/:id", cfg.TemplateLink.Path), handlers.RedirectShortLink)
 	mux.HandleMethodNotAllowed = true
 
-	err := http.ListenAndServe(`:8080`, mux)
+	err := mux.Run(cfg.Addr)
 	if err != nil {
 		panic(err)
 	}
